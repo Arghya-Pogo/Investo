b1f678ed654dc7ab4e0d8a3832b288a2902bf06a
Arghya-Pogo <arghyapogo.28@gmail.com>
2022-09-11 13:19:35 +0530

    Url for posts configured

--- Authentication/views.py
+++ Authentication/views.py
@@ -6,23 +6,26 @@ from .forms import UserRegisterForm
 
 # Create your views here.
 def onBoarding(request):
-    context = {
-        'title': 'Home'
-    }
-    return render(request, 'Authentication/onboarding.html', context)
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        return render(request, 'Authentication/onboarding.html')
 
 
 def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Congratulations {username}, your account has been created! You are now able to log in')
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('dashboard')
     else:
-        form = UserRegisterForm()
-    return render(request, 'Authentication/register.html', {'form': form})
+        if request.method == 'POST':
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'Congratulations {username}, your account has been created! You are now able to log in')
+                return redirect('login')
+        else:
+            form = UserRegisterForm()
+        return render(request, 'Authentication/register.html', {'form': form})
 
 
 @login_required
--- Server/urls.py
+++ Server/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Authentication.urls')),
-    #path('app/', include('Posts.urls')),
+    path('app/', include('Posts.urls')),
     path('djrichtextfield/', include('djrichtextfield.urls'))
 ]
 
